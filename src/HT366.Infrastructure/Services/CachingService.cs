da06b9611bb18aee1ed179e29e32c8d31f81efcf
ngochaidao1999hn <ngochai.dao1999@gmail.com>
2023-09-28 15:41:33 +0700

    Add Redis Cache Service

---
+++ src/HT366.Infrastructure/Services/CachingService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+using System.Text.Json;
+
+namespace HT366.Infrastructure.Services
+{
+    public class CachingService<T> : ICachingService<T> where T : class
+    {
+        private readonly IDistributedCache _cache;
+
+        public CachingService(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public async Task<IEnumerable<T>> GetData(string key)
+        {
+            byte[]? data = await _cache.GetAsync(key);
+            IEnumerable<T>? list = null;
+            if (data != null)
+            {
+                var cachedDataString = Encoding.UTF8.GetString(data);
+                list = JsonSerializer.Deserialize<IEnumerable<T>>(cachedDataString);
+            }
+            return list;
+        }
+
+        public async Task SetData(string key, byte[] data, DistributedCacheEntryOptions options)
+        {
+            await _cache.SetAsync(key, data, options);
+        }
+    }
+}
---
+++ src/HT366.Infrastructure/Services/ICachingService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HT366.Infrastructure.Services
+{
+    public interface ICachingService<T> where T : class
+    {
+        Task<IEnumerable<T>> GetData(string key);
+
+        Task SetData(string key, byte[] data, DistributedCacheEntryOptions options);
+    }
+}
